da2f708507710ee7951f9645e07c8442a54ae25a
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-26 07:18:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,8 +19,10 @@ def nyc_pigeon_organizer(pigeon_data)
           pigeon_list[name][key] = []
         end
         
+      pigeon_list[name][key].push(to_data).to_s  
+      debug.pry
       end
-      pigeon_list[name][key].push(to_data).to_s
+      
     end
   end
   
