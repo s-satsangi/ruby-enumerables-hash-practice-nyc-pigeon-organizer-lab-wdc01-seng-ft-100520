2dbd60ee974c2818c666ddf5b54a2ca0ad020397
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-26 07:31:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
-def nyc_pigeon_organizer(data)
+require 'pry'
+def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
+  
+  #Good Grief.  I spun my wheels on this for more than a week.
+  #After reading about stuff online it looks like maybe I needed
+  #to splat arrays back into the hash to build it the way
+  #I thought made sense, but I couldn't get that working either.  
+  #Implementing the solution from youtube :(
+  
+  pigeon_list = pigeon_data.each_with_object({}) do |(key,value), pigeon_list_arr|
+      
+    value.each do |to_data, names|
+      names.each do |name|
+        #binding.pry
+        if !pigeon_list_arr[name]
+          pigeon_list_arr[name]={}
+        end
+        if !pigeon_list_arr[name][key]
+          pigeon_list_arr[name][key] = []
+        end
+      #binding.pry        
+      pigeon_list_arr[name][key].push(to_data.to_s)
+      #binding.pry
+      end
+      
+    end
+  end
+  #binding.pry
+  #pigeon_list
 end
