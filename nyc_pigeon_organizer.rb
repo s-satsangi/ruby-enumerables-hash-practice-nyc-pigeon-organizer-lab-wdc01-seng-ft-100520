39b50749052a179c676bbef88d18510d1cf22442
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-26 07:03:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,29 +1,17 @@
+
 def nyc_pigeon_organizer(pigeon_data)
   # write your code here!
   
-  #step 1 strip the names out of :gender
-  # & push as string keys on a new hash 
-  # with sub-hash { color:nil, gender:nil, lives:nil}
-  pigeon_name_array = pigeon_data[:gender].values.flatten
-  pigeon_color_array = pigeon_data[:color].keys #.map{|symbol| symbol.to_s}
-  pigeon_gender_array = pigeon_data[:gender].keys.map{|symbol| symbol.to_s}
-  pigeon_lives_array = pigeon_data[:lives].keys
+  #Good Grief.  I spun my wheels on this for more than a week.
+  #After reading about stuff online it looks like maybe I needed
+  #to splat arrays back into the hash to build it the way
+  #I thought made sense, but I couldn't get that working either.  
+  #Implementing the solution from youtube :(
   
-  pigeon_list = pigeon_name_array.map do |name| 
-    name => {
-      :color => pigeon_color_array.each {|symbol|
-                if(pigeon_data[:color][symbol].bsearch{|x| x == name})
-                  symbol.to_s
-                end
-      },
-      :gender => pigeon_gender_array,
-      :lives => [""]
-    }
+  pigeon_data.each_with_object({}) |(key,value), pigeon_list|
+    value.each do
+      
+    end
   end
-  
-  #step 2 for each name in the new hash
-  # cycle through the old data and push
-  # data for color gender & lives into the new hash
-  pigeon_list.each
   
 end
